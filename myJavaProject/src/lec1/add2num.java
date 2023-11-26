8681451c4d2c78dcf524798094dd39e1fe405a3f
Siddhantgupta0809 <siddhantgupta0809@gmail.com>
2023-11-26 17:05:56 +0530

    first commit

---
+++ myJavaProject/src/lec1/add2num.java
@@ -0,0 +1,11 @@
+package lec1;
+
+public class add2num {
+    public static void main(String[] args){
+        int i = 10;
+        int j = 15;
+        int sum = i + j;
+
+        System.out.println("the sum is: " + sum);
+    }
+}
